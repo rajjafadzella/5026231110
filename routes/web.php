6eaade4be69220c2ea6aff62e49a638583566731
rajjafadzella <rajjafadzella@gmail.com>
2025-05-22 15:51:18 +0700

    PERTEMUAN 12

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
 //use itu import java.io kalo di java
 //Illuminate\Support\Facades\ itu package, route itu class
 
 //System.out.println("Hello world")
 // / itu published url, function itu controller
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -62,3 +67,17 @@ Route::get('ets-landingpage', function () {
 Route::get('danantara', function () {
 	return view('danantara');
 });
+
+
+Route::get('dosen',[DosenController::class,'index']);
+Route::get('welcome',[DosenController::class,'welcome']);
+
+Route::get('/pegawai/{nama}',[PegawaiController::class,'index']);
+Route::get('/formulir',[PegawaiController::class,'formulir']);
+Route::post('/formulir/proses',[PegawaiController::class,'proses']);
+
+
+// route blog
+Route::get('/blog', [BlogController::class,'home']);
+Route::get('/blog/tentang', [BlogController::class,'tentang']);
+Route::get('/blog/kontak', [BlogController::class,'kontak']);
